51253a51c1a3b403b564f3c54b637ed6abc6c50c
오병준 <mail@luftaquila.io>
2026-01-27 03:37:58 +0900

    improve: add ofiicial urlparam

--- shared/officialsStore.js
+++ shared/officialsStore.js
@@ -2,7 +2,18 @@ import { ref } from "vue";
 
 const STORAGE_KEY = "fsk_show_officials";
 
-export const showOfficials = ref(localStorage.getItem(STORAGE_KEY) === "true");
+// URL 파라미터 ?official=true 체크
+const urlParams = new URLSearchParams(window.location.search);
+const officialParam = urlParams.get("official") === "true";
+
+// URL 파라미터가 true이면 localStorage에도 저장하고 활성화
+if (officialParam) {
+  localStorage.setItem(STORAGE_KEY, "true");
+}
+
+export const showOfficials = ref(
+  officialParam || localStorage.getItem(STORAGE_KEY) === "true"
+);
 
 export function toggleOfficials() {
   showOfficials.value = !showOfficials.value;
